28d59115e7ce3daa114b3281271bf2635dc57768
Limitless Playing <limitlessplaying@gmail.com>
2024-03-14 21:03:07 +0545

    Added viewsets

---
+++ users/viewsets.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from .serializers import UserSerializer
+
+class userViewsets(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# # if we had defined
+#  class userViewsets(viewsets.Viewset):
+# # we had to define function for each task like
+#     def list(self, request):
+#         //
+#     def retrive(self, request):
+#         //    
