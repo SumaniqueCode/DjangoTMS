dca2aadc42c23e043e3d4d954f2f530a07401ebb
Limitless Playing <limitlessplaying@gmail.com>
2024-03-15 21:41:45 +0545

    automating username creation for user

--- users/serializers.py
+++ users/serializers.py
@@ -4,6 +4,7 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
 
     password = serializers.CharField(write_only=True, required=False) #it has no validations
+    username = serializers.CharField(read_only=True)
 
     def create(self, validated_data):   #for password validation from user model
         password = validated_data.pop('password')
