025fd4a2fd1dcc6e24086568611090d628809bac
Limitless Playing <limitlessplaying@gmail.com>
2024-03-26 21:32:47 +0545

    Customized profile viewsets permissions and added only retrive and update operations using genericviewSet

--- users/viewsets.py
+++ users/viewsets.py
@@ -1,15 +1,18 @@
 from django.contrib.auth.models import User
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 from .serializers import UserSerializer, ProfileSerializer
-from .permissions import IsUserOwnerOrGetAndPostOnly
+from .permissions import IsUserOwnerOrGetAndPostOnly, IsProfileOwnerOrReadOnly
 from .models import Profile
 
 class UserViewsets(viewsets.ModelViewSet):
+    #ModelViewSet contains all CRUD operation so we donot need to them specify all for each opearions
     permission_classes = [IsUserOwnerOrGetAndPostOnly,]
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-class ProfileViewsets(viewsets.ModelViewSet):
+class ProfileViewsets(viewsets.GenericViewSet, mixins.RetrieveModelMixin, mixins.UpdateModelMixin,): 
+    #when we use generic viewsets we can specify specific operations for CRUD using mixins
+    permission_classes = [IsProfileOwnerOrReadOnly,]
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
 
