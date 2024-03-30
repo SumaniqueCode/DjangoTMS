a38c97c00e775e441e89ed94cfbf237fcf40a345
Limitless Playing <limitlessplaying@gmail.com>
2024-03-30 20:37:30 +0545

    added permissions and updated serializers for house app

--- house/router.py
+++ house/router.py
@@ -1,6 +1,7 @@
 from rest_framework import routers
 from .viewsets import HouseViewSets
 
+app_name = "House"
 
 router = routers.DefaultRouter()
 router.register('manage', HouseViewSets)
--- house/viewsets.py
+++ house/viewsets.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets
 from .models import House
 from . serializers import HouseSerializer
+from .permission import IsHouseManagerOrNot
 
 class HouseViewSets(viewsets.ModelViewSet):
     queryset = House.objects.all()
     serializer_class = HouseSerializer
+    permission_classes = [IsHouseManagerOrNot,]
