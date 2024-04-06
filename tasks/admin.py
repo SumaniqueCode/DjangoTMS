981e4ed7e00e00096291441925df962deaf34ee1
Limitless Playing <limitlessplaying@gmail.com>
2024-04-06 21:44:26 +0545

    Added tasklist and attachments models in task model

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-from .models import Task
+from .models import Task, TaskList, Attachments
 
+class TaskListAdmin(admin.ModelAdmin):
+    list_display=('id', 'name')
 class TaskAdmin(admin.ModelAdmin):
     list_display=('id','name', 'description','status', 'created_by', 'completed_by', 'completed_at', 'created_at', 'updated_at', )
 
-admin.site.register(Task, TaskAdmin)
+class TaskAttachmentsAdmin(admin.ModelAdmin):
+    list_display = ('id',)   
+    
+admin.site.register(TaskList, TaskListAdmin)
+admin.site.register(Task, TaskAdmin,)
+admin.site.register(Attachments, TaskAttachmentsAdmin)
--- tasks/models.py
+++ tasks/models.py
@@ -1,13 +1,43 @@
 from django.db import models
+from uuid import uuid4
+from django.utils.deconstruct import deconstructible
+import os
 
 COMPLETED = 'C'
 NOT_COMPLETED = 'NC'
 TASK_STATUS_CHOICES=[(COMPLETED, 'COMPLETED'), (NOT_COMPLETED, 'NOT COMPLETED')]
 
+@deconstructible
+class GenerateAttachmentPath(object):
+    def __init__(self):
+        pass
+    
+    def __call__(self, instance, filename):
+        ext = filename.split( '.')[-1]
+        path = f'media/task/{instance.id}/attachments'
+        name = f'{instance.id}.{ext}'
+        return os.path.join(path, name)
+
+attachment_path = GenerateAttachmentPath()        
+class TaskList(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    completed_on = models.DateTimeField(blank=True, null= True)
+    created_by = models.ForeignKey('users.Profile', on_delete=models.SET_NULL, blank=True, null= True, related_name = 'lists')
+    house = models.ForeignKey('house.House', on_delete=models.CASCADE, blank=True, null=True, related_name='lists')
+    name = models.CharField(max_length=120)
+    description = models.TextField(blank=True, null=True)
+    status = models.CharField(max_length=2, choices=TASK_STATUS_CHOICES, default=NOT_COMPLETED)
+    
+    def __str__(self):
+        return (f'{self.id} | {self.name}')
+    
+
 class Task(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     completed_at = models.DateField(blank=True, null=True)
+    task_list = models.ForeignKey('tasks.TaskList', on_delete=models.CASCADE, related_name='tasks')
     created_by = models.ForeignKey('users.profile', on_delete=models.SET_NULL, blank=True, null=True, related_name = 'created_task')
     completed_by = models.ForeignKey('users.Profile', on_delete=models.SET_NULL, blank=True, null=True, related_name= 'completed_task')
     name = models.CharField(max_length=120)
@@ -17,4 +47,13 @@ class Task(models.Model):
     def __str__(self):
         return (f'{self.id} | {self.name}')
     
-
+class Attachments(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=True)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    data = models.FileField(upload_to=attachment_path)
+    task = models.ForeignKey('tasks.Task',  on_delete= models.CASCADE, related_name='attachments')
+    
+    def __str__(self):
+        return (f'{self.id} | {self.task}')
+    
