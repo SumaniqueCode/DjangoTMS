0b78ef7b71c5aac7e919ad1ebfae7613a0e7e711
Limitless Playing <limitlessplaying@gmail.com>
2024-04-10 22:40:27 +0545

    Added validation to tasklist creation

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -16,6 +16,19 @@ class TaskSerializer(serializers.ModelSerializer):
     completed_by = serializers.HyperlinkedRelatedField(read_only= True, many= False, view_name='profile-detail')
     task_list = serializers.HyperlinkedRelatedField(queryset=TaskList.objects.all(), many = False,view_name='tasklist-detail')
     attachments =serializers.HyperlinkedRelatedField(read_only=True, many=True, view_name='attachments-detail')
+    
+    def validate_task_list(self, value):
+        user_profile = self.context('request').user.profile
+        if value not in user_profile.house.lists.all():
+            raise serializers.ValidationError("Tasklist doesnot belong to the house for which user is member")
+        
+        return value
+    
+    def create(self, validated_data):
+        user_profile = self.context('request').user.profile
+        task = Task.objects.create(**validated_data)
+        task.created_by = user_profile
+        task.save()
     class Meta:
         model = Task
         fields = ['id','url', 'task_list', 'name', 'description','status','attachments',
